5e649f663809d5f689ba54c7080c77bfd6c0c4d8
Oskar Leirvåg <oskar.leir@gmail.com>
2019-02-04 22:42:39 +0100

    change title

--- src/main/java/inf112/skeleton/app/Main.java
+++ src/main/java/inf112/skeleton/app/Main.java
@@ -7,7 +7,7 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 public class Main {
     public static void main(String[] args) {
         LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-        cfg.title = "hello-world";
+        cfg.title = "RoboRally";
         cfg.width = 480;
         cfg.height = 320;
 
