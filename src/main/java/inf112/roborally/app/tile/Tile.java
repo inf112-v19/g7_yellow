28e92a5298eed848976b7c1bf2b96840eb2d6d7c
Hein Josang <ren009@uib.no>
2019-02-05 13:30:27 +0100

    Added dir to tiles

--- src/main/java/inf112/roborally/app/tile/Tile.java
+++ src/main/java/inf112/roborally/app/tile/Tile.java
@@ -2,12 +2,17 @@ package inf112.roborally.app.tile;
 
 public class Tile {
     TileType t;
+    int dir;
 
-    public Tile(TileType t) {
+    public Tile(TileType t, int dir) {
         this.t = t;
+        this.dir = dir;
     }
 
     public TileType getType(){
         return this.t;
+    }
+    public int getDir(){
+        return this.dir;
     }
 }
