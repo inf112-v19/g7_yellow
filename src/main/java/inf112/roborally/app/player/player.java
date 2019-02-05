40ed6da4c0ecc37a988bb9f5fdc0c274aee1b315
Erik Sangolt <neh004@uib.no>
2019-02-05 13:24:41 +0100

    add player

---
+++ src/main/java/inf112/roborally/app/player/player.java
@@ -0,0 +1,21 @@
+package inf112.roborally.app.player;
+
+import java.awt.*;
+
+public class player {
+
+    private int id;
+    private int posX;
+    private int posY;
+    private int health;
+    private int direction;
+
+    public player(int id, Point pos){
+        this.id = id;
+        this.posX = pos.x;
+        this.posY = pos.y;
+    }
+
+
+
+}
