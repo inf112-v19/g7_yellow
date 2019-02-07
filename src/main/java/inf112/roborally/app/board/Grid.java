dbfef0a65f46dc16cbc04c1d6f37e20d7640de35
Oskar Leirvåg <oskar.leir@gmail.com>
2019-02-07 11:18:41 +0100

    Change from int x,y to Vector 2
    > Also made a new exception "OutsideGridException"
    > Change the cells from IBoardTile to LinkedList<IBoardTile>

--- src/main/java/inf112/roborally/app/board/Grid.java
+++ src/main/java/inf112/roborally/app/board/Grid.java
@@ -1,54 +1,64 @@
 package inf112.roborally.app.board;
 
+import com.badlogic.gdx.math.Vector2;
+import inf112.roborally.app.exceptions.OutsideGridException;
 import inf112.roborally.app.tile.IBoardTile;
+
+import java.util.LinkedList;
 
 public class Grid {
 
     private int width, height;
-    private IBoardTile[][] tiles;
+    private LinkedList<IBoardTile>[][] tiles;
 
     public Grid(int width, int height) {
-        tiles = new IBoardTile[width][height];
+        tiles = new LinkedList[width][height];
         this.width = width;
         this.height = height;
     }
 
     /**
      * Add new tile at position x,y. Will replace old tile if any.
-     * @param x position x
-     * @param y position y
-     * @param tile The tile to be added
+     * @param p
+     * @param tile
+     * @throws OutsideGridException
      */
-    public void addTile(int x, int y, IBoardTile tile) {
-        if (x > width || x < 0 || y > height || y < 0)
-            throw new IllegalArgumentException("Outside of map!");
-        tiles[x][y] = tile;
+    public void addTile(Vector2 p, IBoardTile tile) throws OutsideGridException {
+        var tileList = getTiles(p);
+        tileList.add(tile);
     }
 
     /**
      * Get tile at given position
-     * @param x position x
-     * @param y position y
+     *
+     * @param p
      * @return
      */
-    public IBoardTile getTile(int x, int y) {
-        if (x > width || x < 0 || y > height || y < 0)
-            throw new IllegalArgumentException("Outside of map!");
-        return tiles[x][y];
+    public LinkedList<IBoardTile> getTiles(Vector2 p) throws OutsideGridException{
+        if (isInsideGrid(p))
+            throw new OutsideGridException(p, "Outside of map!");
+
+        if (tiles[(int) p.x][(int) p.y] == null)
+            tiles[(int) p.x][(int) p.y] = new LinkedList<>();
+
+        return tiles[(int) p.x][(int) p.y];
     }
 
     /**
-     *
      * Remove a tile at position x,y.
-     * @param x position x
-     * @param y position y
+     *
+     * @param p
+     * @param tile
      * @return The removed tile
      */
-    public IBoardTile removeTile(int x, int y, IBoardTile tile) {
-        if (x > width || x < 0 || y > height || y < 0)
-            throw new IllegalArgumentException("Outside of map!");
-        IBoardTile tileToRemove = getTile(x,y);
-        tiles[x][y] = null; //TODO: replace with null when Tile is created
-        return tileToRemove;
+    public void removeTile(Vector2 p, IBoardTile tile) throws OutsideGridException {
+        var tilesAtPos = getTiles(p);
+        tilesAtPos.remove(tile);
     }
+
+    private boolean isInsideGrid(Vector2 p) {
+        return p.x > width || p.x < 0 || p.y > height || p.y < 0;
+    }
+
+
 }
