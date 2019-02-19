23636c80a4f2a045cb58116252115e6c343f2c58
Andrè F. Jønland <andrefroseth@hotmail.com>
2019-02-19 13:29:07 +0100

    made AbstractCardPile generic

--- src/main/java/inf112/roborally/app/helpers/AbstractCardPile.java
+++ src/main/java/inf112/roborally/app/helpers/AbstractCardPile.java
@@ -7,5 +7,15 @@ package inf112.roborally.app.helpers;
  * @version 1.0
  * @since 19.02.19
  */
-public abstract class AbstractCardPile implements ICardPile {
+public abstract class AbstractCardPile<E> implements ICardPile<E> {
+
+    @Override
+    public void add(E card) {
+        
+    }
+
+    @Override
+    public E pop() {
+        return null;
+    }
 }
