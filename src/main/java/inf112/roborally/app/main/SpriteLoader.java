769667d3a95149a56a1608d533d6cad8be0333f4
Hein Josang <ren009@uib.no>
2019-02-21 15:14:43 +0100

    Trying basic SpriteLoader

--- src/main/java/inf112/roborally/app/main/SpriteLoader.java
+++ src/main/java/inf112/roborally/app/main/SpriteLoader.java
@@ -1,22 +1,39 @@
 package inf112.roborally.app.main;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import inf112.roborally.app.tile.Floor;
 import inf112.roborally.app.tile.Hole;
 import inf112.roborally.app.tile.IBoardTile;
 
+import java.io.InputStream;
+
 public class SpriteLoader {
 
+    final String SPRITE_PATH = this.getClass().getPackageName().replace('.', '/') + '/';
+
     Sprite floorSprite;
+    Sprite holeSprite;
 
     public SpriteLoader() {
         loadSprites();
     }
 
 
-    private void loadSprites() {
 
+    private void loadSprites() {
+        //InputStream in = null;
+        //foreach(??){
+        Texture img = new Texture(SPRITE_PATH + "Hole.png");
+        floorSprite = new Sprite(img);
+        floorSprite.setSize(Main.TILE_SIZE, Main.TILE_SIZE);
+
+        Texture img2 = new Texture(SPRITE_PATH + "Floor.png");
+        floorSprite = new Sprite(img);
+        floorSprite.setSize(Main.TILE_SIZE, Main.TILE_SIZE);
+        //}
     }
+
 
     public Sprite getSprite(IBoardTile t) {
         if (t instanceof Hole) {
