d14f3e0fd1bffcc3a91607cae7518785a5ed3fb4
Andrè F. Jønland <andrefroseth@hotmail.com>
2019-02-26 13:19:17 +0100

    Added remove test for cards

--- src/test/java/inf112/roborally/app/CardTest.java
+++ src/test/java/inf112/roborally/app/CardTest.java
@@ -18,5 +18,18 @@ public class CardTest {
         System.out.println(pile.size());
         assertEquals(2, pile.size());
     }
+
+    @Test
+    public void doesItRemove() {
+        pile = new AbstractCardPile<Integer>();
+
+        for(int i = 0; i<10; i++) {
+            pile.add(i);
+        }
+        pile.pop();
+        System.out.println(pile.toString());
+
+        assertEquals(9, pile.size());
+    }
 }
 
