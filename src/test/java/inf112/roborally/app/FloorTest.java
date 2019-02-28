d0154fe3c8060fb584198d5ab21ac1fc2e22525f
Erik Sangolt <neh004@uib.no>
2019-02-28 10:44:05 +0100

    add value to robot and floor

--- src/test/java/inf112/roborally/app/FloorTest.java
+++ src/test/java/inf112/roborally/app/FloorTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.*;
 
 public class FloorTest {
 
-    private Floor floor = new Floor();
+    private Floor floor = new Floor(90);
 
     @Test
     public void renderPriorityTest(){
