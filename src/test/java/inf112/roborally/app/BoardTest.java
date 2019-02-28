85b787bfa8eedd01ec8bd0f88d2f38e9d36863a1
Daniel <daniel.silgjerd@gmail.com>
2019-02-28 11:17:34 +0100

    More board tests

--- src/test/java/inf112/roborally/app/BoardTest.java
+++ src/test/java/inf112/roborally/app/BoardTest.java
@@ -1,7 +1,10 @@
 package inf112.roborally.app;
 
 
+import com.badlogic.gdx.math.Vector2;
 import inf112.roborally.app.board.Board;
+import inf112.roborally.app.exceptions.OutsideGridException;
+import inf112.roborally.app.tile.Hole;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -14,7 +17,7 @@ public class BoardTest {
     private Board board = new Board(1,1);
 
     @Test
-    public void getGridTest(){
+    public void GetGridTest(){
         assertNotNull(board.getGrid());
     }
 
@@ -38,5 +41,16 @@ public class BoardTest {
         assertNotEquals(2, board.getWidth());
     }
 
+    @Test
+    public void TestLoadEmptyMapIsEmpty() throws OutsideGridException {
+        board.loadEmptyMap();
+        assertEquals(board.getGrid().getTiles(new Vector2(0,0)).size(), 0);
+    }
+
+    @Test
+    public void TestLoadDefaultMapWorks() throws OutsideGridException {
+        board.loadDefaultMap();
+        assertNotEquals(board.getGrid().getTiles(new Vector2(0,0)).size(), 0);
+    }
 
 }
