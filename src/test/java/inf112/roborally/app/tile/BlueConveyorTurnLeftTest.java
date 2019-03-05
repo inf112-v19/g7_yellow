3862e7fca399a9188faf0b57e8678b243efa9faf
Hein Josang <ren009@uib.no>
2019-03-05 10:10:49 +0100

    Finally all tiles done with JaCoCo

---
+++ src/test/java/inf112/roborally/app/tile/BlueConveyorTurnLeftTest.java
@@ -0,0 +1,33 @@
+package inf112.roborally.app.tile;
+
+import com.badlogic.gdx.math.Vector2;
+import inf112.roborally.app.player.Player;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BlueConveyorTurnLeftTest {
+
+    BlueConveyorTurnLeft bC = new BlueConveyorTurnLeft(90);
+    Player p = new Player(1,new Vector2(0,0), 0);
+
+
+    @Test
+    public void getRenderPriority() {
+        assertEquals(2,bC.getRenderPriority());
+    }
+
+    @Test
+    public void getSymbol() {
+        assertEquals('X', bC.getSymbol());
+    }
+
+    @Test
+    public void execute() {
+        Player temp = p;
+        bC.execute(temp);
+        bC.execute(temp);
+        assertEquals(new Vector2(0,2), temp.getPos());
+    }
+
+}
