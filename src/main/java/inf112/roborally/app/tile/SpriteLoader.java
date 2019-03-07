11e7bb622f65aa398b21e9f5ce009c3f91c8c631
Oskar Leirvåg <oskar.leir@gmail.com>
2019-03-08 00:35:56 +0100

    Change loadSprites to handle JAR-encodin
    > Tests if inside jarfile
    > If inside will now add all entries from jar methods

--- src/main/java/inf112/roborally/app/tile/SpriteLoader.java
+++ src/main/java/inf112/roborally/app/tile/SpriteLoader.java
@@ -1,17 +1,22 @@
 package inf112.roborally.app.tile;
+
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import inf112.roborally.app.main.Main;
 
 import java.io.File;
-import java.util.Iterator;
-import java.util.LinkedList;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URISyntaxException;
+import java.net.URLDecoder;
+import java.util.*;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 public class SpriteLoader {
 
     private final String SPRITE_PATH = this.getClass().getPackageName().replace('.', '/') + '/';
-    private final String RESOURCE_PATH = "src/main/resources/";
     private static Sprite MISSING_SPRITE;
 
     private static LinkedList<SpriteContainer> list = new LinkedList<>();
@@ -22,30 +27,61 @@ public class SpriteLoader {
 
     /**
      * This is pretty untestable so I just let it be without a test
+     *
      * @param t IBoardTile to fetch the sprite of
      * @return the sprite if found, MISSING_SPRITE otherwise
      */
     public static Sprite fetchSprite(IBoardTile t) {
         Iterator<SpriteContainer> it = list.iterator();
-        while(it.hasNext()){
+        while (it.hasNext()) {
             SpriteContainer sC = it.next();
-            if(t.toString().equals(sC.getName())) return sC.getSprite();
+            if (t.toString().equals(sC.getName())) return sC.getSprite();
         }
         return MISSING_SPRITE;
     }
 
     private void loadSprites() {
 
-        File folder = new File(RESOURCE_PATH + SPRITE_PATH);
-        File[] listOfFiles = folder.listFiles();
+        try {
+            var uri = this.getClass().getClassLoader().getResource(SPRITE_PATH).toURI();
 
-        for (int i = 0; i < listOfFiles.length; i++) {
-            if (listOfFiles[i].isFile()) {
-                Texture texture = new Texture(new FileHandle (listOfFiles[i]));
-                Sprite sprite = new Sprite(texture);
-                sprite.setSize(Main.TILE_SIZE, Main.TILE_SIZE);
-                list.add(new SpriteContainer(sprite, listOfFiles[i].getName()));
+            //TEST TO SEE IF INSIDE JAR
+            if (uri.toString().contains("jar:")) {
+                String jarPath = uri.toString().substring(9, uri.toString().indexOf("!")); //strip out only the JAR file
+                JarFile jar = new JarFile(URLDecoder.decode(jarPath, "UTF-8"));
+
+                Enumeration<JarEntry> entries = jar.entries(); //gives ALL entries in jar
+                Set<String> result = new HashSet<>();
+
+                //Add all files to set as to remove dupes
+                entries.asIterator().forEachRemaining(x -> {
+                    if (x.toString().matches("inf112/roborally/app/tile/{1}.*\\.png")) {
+                        result.add(x.getName());
+                    }
+                });
+
+                //Foreach file
+                result.forEach(x -> {
+                    Texture texture = new Texture(x);
+                    Sprite sprite = new Sprite(texture);
+                    sprite.setSize(Main.TILE_SIZE, Main.TILE_SIZE);
+                    var hold = x.split("/");
+                    list.add(new SpriteContainer(sprite, hold[hold.length - 1]));
+                });
+
+            } else {
+                File[] listOfFiles = new File(uri).listFiles();
+                for (File f : listOfFiles) {
+                    if (f.isFile() && f.toString().contains(".png")) {
+                        Texture texture = new Texture(new FileHandle(f));
+                        Sprite sprite = new Sprite(texture);
+                        sprite.setSize(Main.TILE_SIZE, Main.TILE_SIZE);
+                        list.add(new SpriteContainer(sprite, f.getName()));
+                    }
+                }
             }
+        } catch (URISyntaxException | IOException e) {
+            e.printStackTrace();
         }
 
         Texture img = new Texture(SPRITE_PATH + "MISSING_SPRITE.png");
