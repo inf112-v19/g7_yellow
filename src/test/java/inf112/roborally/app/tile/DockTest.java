1dc46d208ff7cd9a16e97b0b196bfd58d582fa30
Erik Sangolt <neh004@uib.no>
2019-04-01 11:38:53 +0200

    add Docktest

---
+++ src/test/java/inf112/roborally/app/tile/DockTest.java
@@ -0,0 +1,18 @@
+package inf112.roborally.app.tile;
+
+import inf112.roborally.app.tile.tiles.Dock;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class DockTest {
+
+    private Dock dock;
+
+    @Test
+    public void setAndGetIdTest(){
+        dock = new Dock(1);
+        dock.setId(1);
+        assertEquals(dock.getId(),1);
+    }
+
+}
