57be1b5041f238cb3d1b7c8a56c45f306bdd722e
Erik Sangolt <neh004@uib.no>
2019-04-01 11:40:17 +0200

    add robot test

---
+++ src/test/java/inf112/roborally/app/tile/RobotTest.java
@@ -0,0 +1,31 @@
+package inf112.roborally.app.tile;
+
+import inf112.roborally.app.tile.tiles.Robot;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+public class RobotTest {
+
+    private Robot robot;
+
+    @Test
+    public void canMoveIntoFromTest(){
+        robot = new Robot(1);
+        assertFalse(robot.canMoveIntoFrom(90));
+    }
+
+    @Test
+    public void canMoveOutFromTest(){
+        robot = new Robot(1);
+        assertFalse(robot.canMoveOutFrom(90));
+    }
+
+    @Test
+    public void getIdTest(){
+        robot = new Robot(1);
+        assertEquals(robot.getId(),0);
+    }
+
+}
