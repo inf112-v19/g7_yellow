077a49abaf2e0f673ad96d6c2a69c34c2dfe19d1
Oskar Leirvåg <oskar.leir@gmail.com>
2019-04-01 13:27:51 +0200

    Add MapLayoutTests

--- src/test/java/inf112/roborally/app/MapLayoutTest.java
+++ src/test/java/inf112/roborally/app/MapLayoutTest.java
@@ -1,11 +1,36 @@
 package inf112.roborally.app;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import inf112.roborally.app.main.MapLayout;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
 /**
- * TODO: Describe class
+ * Small test to start the MapLayout
+ * class in fake environment
  *
  * @author RakNoel
  * @version 1.0
  * @since 01.04.19
  */
 public class MapLayoutTest {
+
+    @BeforeAll
+    public static void init() {
+        Gdx.gl20 = Mockito.mock(GL20.class);
+        Gdx.gl = Gdx.gl20;
+    }
+
+    @Test
+    public void justStart() {
+        var tester = Mockito.mock(MapLayout.class);
+        var maplayout = new MapLayout();
+
+        tester.resize(0,0);
+        tester.pause();
+        tester.resume();
+        tester.create();
+    }
 }
