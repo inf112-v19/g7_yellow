396fe627f3517f201aa87f0de08b4f6e08f1ed4e
Erik Sangolt <neh004@uib.no>
2019-05-03 15:10:53 +0200

    add imagebuttons to menu

--- src/main/java/inf112/roborally/app/main/Menu.java
+++ src/main/java/inf112/roborally/app/main/Menu.java
@@ -4,16 +4,16 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Button;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 
 import static inf112.roborally.app.editor.Console.clear;
 
@@ -30,9 +30,10 @@ public class Menu implements Screen {
         stage = new Stage();
         table = new Table();
         table.setFillParent(true);
+
+        //shows lines
         //table.setDebug(true);
         stage.addActor(table);
-
         TextButton.TextButtonStyle style = new TextButton.TextButtonStyle();
         Label.LabelStyle labelStyle = new Label.LabelStyle();
         new Button.ButtonStyle();
@@ -43,37 +44,41 @@ public class Menu implements Screen {
         labelStyle.font = font;
         labelStyle.fontColor = Color.RED;
 
-        Label roborally = new Label("RoboRally", labelStyle);
-        roborally.setPosition((Main.WINDOW_WIDTH / 2) - 70,(Main.WINDOW_HEIGHT) - 300);
-        stage.addActor(roborally);
+        Texture robo = new Texture(Gdx.files.internal("src/main/resources/inf112/roborally/app/Menu/RoboMenu.png"));
+        Texture editor = new Texture(Gdx.files.internal("src/main/resources/inf112/roborally/app/Menu/editor1.png"));
+        Texture play = new Texture(Gdx.files.internal("src/main/resources/inf112/roborally/app/Menu/play3.png"));
+        Texture exit = new Texture(Gdx.files.internal("src/main/resources/inf112/roborally/app/Menu/exit1.png"));
 
-        //create buttons
-        TextButton newGame = new TextButton("Play", style);
-        TextButton editor = new TextButton("Editor", style);
-        TextButton exit = new TextButton("Exit", style);
 
-        //add to table
-        table.add(newGame);
+        ImageButton rr = new ImageButton(new TextureRegionDrawable(new TextureRegion(robo)));
+        ImageButton playbutton = new ImageButton(new TextureRegionDrawable(new TextureRegion(play)));
+        ImageButton editorbutton = new ImageButton(new TextureRegionDrawable(new TextureRegion(editor)));
+        ImageButton exitbutton = new ImageButton(new TextureRegionDrawable(new TextureRegion(exit)));
+
+        //adds buttons to table
+        table.add(rr).center().size(600,300);
+        table.row().padBottom(10);
+        table.add(playbutton).height(0).height(100);
+        table.row().padBottom(10);
+        table.add(editorbutton).height(0).height(100);
         table.row();
-        table.add(editor);
-        table.row();
-        table.add(exit);
+        table.add(exitbutton).height(0).height(100);
 
-        newGame.addListener(new ChangeListener() {
+        playbutton.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent changeEvent, Actor actor) {
                 Main.gameState = GameState.PLAYING;
             }
         });
 
-        editor.addListener(new ChangeListener() {
+        editorbutton.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
                 Main.gameState = GameState.EDITOR;
             }
         });
 
-        exit.addListener(new ChangeListener() {
+        exitbutton.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent changeEvent, Actor actor) {
                 Gdx.app.exit();
@@ -82,16 +87,16 @@ public class Menu implements Screen {
 
         Gdx.input.setInputProcessor(stage);
 
-
-
     }
 
 
     @Override
     public void render(float v) {
         if(active && Main.gameState == GameState.MENU){
+            Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
             stage.act();
             stage.draw();
+
         }
     }
 
